7993e26edc5eaec13c15969dd008420022dd0a71
MirkOrlando <mirko@orlando-m.it>
2022-04-21 16:39:11 +0200

    created function timerChange BONUS 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,8 @@ in evidenza rispetto alle altre.
 
 Bonus:
 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
-2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+2- applicare l'autoplay allo slider: 
+   ogni 3 secondi, cambia immagine automaticamente
 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 */
 
@@ -81,7 +82,13 @@ const app = new Vue(
             getActive(index) {
                 //console.log('get active', index);
                 this.activeLocation = index
+            },
+            timerNext() {
+                setInterval(this.getNext, 3000);
             }
+        },
+        created() {
+            this.timerNext()
         }
     }
 )
